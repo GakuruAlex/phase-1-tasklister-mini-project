a6f2f81966e76f350137a7fa18ba724feedb5a70
Alex Gicimo Gakuru <gakurualexgicimo@gmail.com>
2022-08-23 09:07:30 +0300

    Assignment Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let myForm=document.querySelector(`form`);
+  myForm.addEventListener(`submit`,(e)=>{
+ e.preventDefault();
+handleToDo(e.target.new_task_description.value);
+
+ myForm.reset();
+
+  }
+
+  )
 });
+
+
+ function handleToDo(todo){
+  let myList=document.createElement(`li`);
+  let btn =document.createElement(`button`);
+  btn.addEventListener(`click`,deleteButton)
+  myList.textContent=`${todo} `;
+  btn.textContent=` x`;
+
+  myList.appendChild(btn);
+
+  console.log(myList)
+  document.querySelector(`#tasks`).appendChild(myList);
+
+ }
+
+ function deleteButton(e){
+
+ e.target.parentNode.remove();
+
+ }
+
